fa9263d8b7237a124e4a56a1f34c4537394d6a48
Shubham Khandelwal <shubham.khandelwal@shubham.khandelwal-TJLJTCD9J0>
2023-05-25 02:48:34 +0530

    Backend for microsoft teams

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const {v4 : uuidv4} = require('uuid')
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+
+const fs = require("fs");
+require("dotenv").config();
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+  })
+);
+
+// fetch all the users 
+app.get("/getAllUsers", (req, res) => {
+    const file = fs.readFileSync("./Database/users.json", {
+      encoding: "utf8",
+      flag: "r",
+    });
+    const userData = JSON.parse(file);
+  
+    if (userData) res.send(userData);
+    res.send(new Error("User not found"));
+  });
+
+
+//fetch currentUser's messages
+app.get("/getUserMessages/:id", (req, res) => {
+  const file = fs.readFileSync("./Database/userMessages.json", {
+    encoding: "utf8",
+    flag: "r",
+  });
+  const userData = JSON.parse(file);
+
+  const currentUser = userData.find((ele) => {
+    return ele.id === req.params.id;
+  });
+
+  if (currentUser) res.send(currentUser);
+  res.send(new Error("User not found"));
+});
+
+// Add message to list
+
+app.put("/addMessage/:userId", (req, res) => {
+
+    let userMessages = fs.readFileSync("./Database/userMessages.json", {
+        encoding: "utf8",
+        flag: "r",
+    });
+
+    //data of post request
+    const data = req.body;
+
+    const userMessagesData = JSON.parse(userMessages)
+    const currentUserMessages = userMessagesData.find((userMessage) => {
+        return userMessage.id === req.params.userId
+    })
+    currentUserMessages.messages.push(data)
+
+    userMessages = userMessagesData.map((userMessage)=> {
+        if(userMessage.id === req.params.userId) return currentUserMessages
+        return userMessage
+    })
+
+    fs.writeFileSync("./database/userMessages.json", JSON.stringify(userMessages));
+    res.json(data);
+
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
